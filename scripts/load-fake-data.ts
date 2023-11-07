8d218ba24e34f7aad7ffd4ae9b134ca96a458581
Diovan Leal <diovan_dol@hotmail.com>
2023-11-07 08:03:24 -0300

    complete load fake data script

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -28,6 +28,35 @@ async function loadFakeData(numUsers: number) {
       );
     }
 
+    const res = await client.query(
+      "select id from public.users order by created_at desc limit $1",
+      [numUsers]
+    );
+
+    console.log(res.rows);
+
+    for (const row of res.rows) {
+      for (let i = 0; i < Math.ceil(Math.random() * 50); i++) {
+        await client.query(
+          "insert into public.posts (user_id, content) values ($1, $2)",
+          [row.id, faker.lorem.sentence()]
+        );
+      }
+    }
+
+    for (const row1 of res.rows) {
+      for (const row2 of res.rows) {
+        if (row1.id != row2.id) {
+          if (Math.random() > 0.5) {
+            await client.query(
+              "insert into follows (user_id, follower_id) values ($1, $2)",
+              [row1.id, row2.id]
+            );
+          }
+        }
+      }
+    }
+
     await client.query("commit");
   } catch (error) {
     await client.query("rollback");
