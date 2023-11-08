b28ccffb1987aca3d06ca83a9fff3bef8c725546
Diovan Leal <diovan_dol@hotmail.com>
2023-11-08 08:43:58 -0300

    login endpoint

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,7 @@
 import { sql } from "@/db";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
 
 export async function POST(request: Request) {
     const json = await request.json();
@@ -9,7 +11,48 @@ export async function POST(request: Request) {
         [json.username]
     );
 
-    return NextResponse.json({
-        data: res.rows[0]
+    if (res.rowCount == 0) {
+        return NextResponse.json(
+            {
+                error: "user not found"
+            },
+            {
+                status: 404
+            }
+        );
+    }
+
+    const user = res.rows[0];
+
+    const match = await bcrypt
+        .compare(json.password, user.password);
+
+    if (!match) {
+        return NextResponse.json(
+            {
+                error: "invalid credentials"
+            },
+            {
+                status: 401
+            }
+        );
+    }
+
+    const token = await new SignJWT({})
+        .setProtectedHeader({alg: "HS256"})
+        .setSubject(user.id)
+        .setIssuedAt()
+        .setExpirationTime("2w")
+        .sign(new TextEncoder()
+            .encode("my-jwt-secret"));
+
+    const response = NextResponse.json({msg: "login sucess"});
+
+    response.cookies.set("jwt-token", token, {
+        sameSite: "strict", // prevent cross-site vulnerabilities
+        httpOnly: true,
+        secure: true,
     });
+
+    return response;
 }
