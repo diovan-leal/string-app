4b85d87042cf8796a747754fd42512b8950d5ada
Diovan Leal <diovan_dol@hotmail.com>
2023-11-08 07:06:32 -0300

    refact db

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,18 +1,13 @@
-import { getClient } from "@/db";
+import { sql } from "@/db";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     const json = await request.json();
-    const client = await getClient();
-    
-    await client.connect();
-    
-    const res = await client.query(
+
+    const res = await sql(
         "select id, username, password from users where username ilike $1",
         [json.username]
     );
-
-    await client.end();
 
     return NextResponse.json({
         data: res.rows[0]
