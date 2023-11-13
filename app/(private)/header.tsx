8734c753fcce88fb209779fcd0292753ec54fa23
Diovan Leal <diovan_dol@hotmail.com>
2023-11-13 09:04:37 -0300

    swr and fecther

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,3 +1,31 @@
+"use client";
+import useSWR from "swr";
+
+const fetcher = async (url: RequestInfo | URL) => {
+    const res = await fetch(url);
+    if (!res.ok) {
+        const msg = "An error occured while fetching the data.";
+        const info = await res.json();
+        const status = res.status;
+        const error = new Error(msg);
+        console.error(info, status);
+        throw error;
+    }
+    return res.json();
+};
+
 export default function Header() {
+    const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+
+    if (error) {
+        return <div>failed to load</div>;
+    }
+
+    if (isLoading) {
+        return <div>loading...</div>;
+    }
+
+    console.log(data);
+
     return <header>Header</header>;
 }
