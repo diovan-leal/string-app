a130299ef0b1f4a8fb11a74774790a210c41b7fd
Diovan Leal <diovan_dol@hotmail.com>
2023-11-14 08:16:03 -0300

    header user component

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -2,6 +2,7 @@
 
 import useSWR from "swr";
 import fetcher from "../util/fetcher";
+import User from "../components/user";
 
 export default function Header() {
     const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
@@ -16,5 +17,14 @@ export default function Header() {
 
     console.log(data);
 
-    return <header>Header</header>;
+    return (
+        <header className="flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justiy-between items-center">
+            <div>
+                <h1 className="font-mono text-lg">Strings</h1>
+            </div>
+            <div>
+                {<User user={data.data} href="account"/>}
+            </div>
+        </header>
+    );
 }
