aeed2db373735061da130530f1ebf6034f0bcc4c
Diovan Leal <diovan_dol@hotmail.com>
2023-11-14 08:35:55 -0300

    style navbar and footer

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
 export default function NavBar() {
-    return <nav>navBar</nav>;
+    return (
+        <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg my-2">
+            <ul className="flex flex-row justify-around w-full">
+                <li>
+                    <Link href="/feed"
+                        className="">Feed</Link>
+                </li>
+                <li>
+                    <Link href="/profile"
+                        className="">Profile</Link>
+                </li>
+                <li>
+                    <Link href="/following"
+                        className="">Following</Link>
+                </li>
+                <li>
+                    <Link href="/followers"
+                        className="">Followers</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
