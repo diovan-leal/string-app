55f99c4fbd2458ec41f2574a19faa4732beadeb2
Diovan Leal <diovan_dol@hotmail.com>
2023-11-17 08:38:54 -0300

    stylling the feed page

---
+++ app/(private)/feed/feed-container.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import FeedList from "./feed-list";
+
+function FeedContainer() {
+    const [cnt, setCont] = useState(1);
+    const pages = [];
+
+    for (let i = 0; i < cnt; i++) {
+        pages.push(<FeedList index={i} key={i} />);
+    }
+
+    return (
+        <div>
+            {pages}
+            <div className="flex justify-center">
+                <button className="bg-slate-900 p-2 rounded-lg self-center"
+                    onClick={() => setCont(cnt + 1)}>Load more</button>
+            </div>
+        </div>
+    );
+}
+
+export default FeedContainer;
