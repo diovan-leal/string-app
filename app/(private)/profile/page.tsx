db96acf3e850ef8710784576a9ad75f5d77fcb07
Diovan Leal <diovan_dol@hotmail.com>
2023-12-06 09:37:41 -0300

    profile page, post, container e form

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,7 +1,25 @@
-export default async function Frofile() {
+"use client";
+import useSWR from "swr";
+import Form from "./form";
+import PostContainer from "@/app/components/post-container";
+
+export default function Frofile() {
+    const {data, error, isLoading} = useSWR("/api/users/profile");
+
+    if (error) {
+        return <div>failed to load</div>
+    }
+
+    if (isLoading) {
+        return <div>loading...</div>
+    }
+
+    console.log(data);
     return (
         <main>
             <h2>Profile</h2>
+            <Form />
+            <PostContainer username={data.data.username} />
         </main>
     );
 }
