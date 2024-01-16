cb5d3ea271143cf426b3aeb85319b25363cad005
Diovan Leal <diovan_dol@hotmail.com>
2024-01-16 08:02:12 -0300

    account page

---
+++ app/(private)/account/signout-btn.tsx
@@ -0,0 +1,20 @@
+export default function SignOutButton() {
+    const router = useRouter();
+
+    async function handleSignOut() {
+        const res = await fetch("/api/logout");
+
+        if (res.ok) {
+            router.push("/signin")
+        }
+    }
+
+    return (
+        <button
+            onClick={handleSignOut}
+            className="text-green-400 underline p-2 rounded-lg my-5"
+        >
+            Sign Out
+        </button>
+    )
+}
