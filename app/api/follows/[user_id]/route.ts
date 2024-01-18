e3aeb10af86a164bc4e480bebf358346f32e9d73
Diovan Leal <diovan_dol@hotmail.com>
2024-01-17 23:06:50 -0300

    delete follow endpoint

---
+++ app/api/follows/[user_id]/route.ts
@@ -0,0 +1,17 @@
+import { getJWTPayLoad } from "@/app/util/auth";
+import { sql } from "@/db";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+    request: Request,
+    { params }: { params: { user_id: number } }
+) {
+    const jwtPayload = await getJWTPayLoad();
+    const userId = params.user_id;
+    await sql(
+        "delete from follows where user_id = $1 and follower_id = $2",
+        [userId, jwtPayload.sub]
+    );
+    
+    return NextResponse.json({ msg: "follow deleted" });
+} 
