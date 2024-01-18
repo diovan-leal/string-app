0c92a37878817734a688ae5e545fd96c5b2a12d0
Diovan Leal <diovan_dol@hotmail.com>
2024-01-17 23:51:28 -0300

    user page header

---
+++ app/(private)/[username]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import UserPageHeader from "./user-page-header"
+
+export default function UserPage({ params }: {params : {username: string}}) {
+    
+
+    return (
+        <div>
+            <UserPageHeader username={params.username} />
+            <div>post container {params.username}</div>
+        </div>
+    )
+}
