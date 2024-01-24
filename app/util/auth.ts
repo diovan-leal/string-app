51ecc09ee0c3d27b8a8e724590c94a7b0f27b163
Diovan Leal <diovan_dol@hotmail.com>
2024-01-24 09:04:46 -0300

    flag misinformation endpoint authorization

--- app/util/auth.ts
+++ app/util/auth.ts
@@ -1,5 +1,7 @@
+import { sql } from "@/db";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 export async function getJWTPayLoad() {
     const cookieStore = cookies();
     const token = cookieStore.get("jwt-token");
@@ -10,4 +12,23 @@ export async function getJWTPayLoad() {
     const { payload, protectedHeader } = await jwtVerify(token?.value!, secret);
     console.log(2, payload);
     return payload;
+}
+
+export async function authorizedAdmin(func: Function) {
+    const jwtPayload = await getJWTPayLoad();
+    const res = await sql("select is_admin from users where id = $1", [jwtPayload.sub]);
+    const data = res.rows[0];
+
+    if (!data.is_admin) {
+        return NextResponse.json(
+            {
+                error: "unauthorized"
+            },
+            {
+                status: 403
+            }
+        );
+    }
+
+    return func();
 }
