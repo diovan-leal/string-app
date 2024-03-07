0ea641474d874f615f8d51140efb1e31ac53d130
Diovan Leal <diovan_dol@hotmail.com>
2024-03-07 08:42:37 -0300

    fix collumn name

--- app/api/cron/delete-misinformation-job/route.ts
+++ app/api/cron/delete-misinformation-job/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     console.log("executing delete misinformation job");
 
     const res = await sql(
-        "delete from posts where is_misinformation = true and is_misinformartion_flagged_at <= now() - interval '1 minute'"
+        "delete from posts where is_misinformation = true and is_misinformation_flagged_at <= now() - interval '1 minute'"
     );
 
     return NextResponse.json({
